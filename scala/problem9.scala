c03b0e6eef7732817a7d87e653a87649c30d38d5
Eneko Alonso <eneko.alonso@gmail.com>
2013-02-16 15:46:21 -0800

    Scala solution for problem 9

--- scala/problem9.scala
+++ scala/problem9.scala
@@ -7,13 +7,9 @@ import util.control.Breaks._
 object Problem9 extends App
 {
   override def main(args: Array[String]) {
-    var a=1
-    var b=2
-    var c=3
-
     breakable {
-      for (a <- 1 to 333) {
-        for (b <- (a+1) to 500) {
+      for (a <- 1 to 332) {
+        for (b <- a+1 to 498) {
           var c = 1000 - a - b
           println(a,b,c)
           if (a*a + b*b == c*c) {
